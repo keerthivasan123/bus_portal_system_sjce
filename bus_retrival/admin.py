6d66929ce2949399db426112bfa17ce5c569d3fb
keerthivasan123 <keerthivasan287@gmail.com>
2019-07-24 22:26:48 +0530

    added weekly Expenses in admin model

--- bus_retrival/admin.py
+++ bus_retrival/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Mechanical,Electrical,spareExpenses
+from .models import Mechanical,Electrical,spareExpenses,weeklyExpenses,tyres,dieselFilling
 
 admin.site.register(spareExpenses)
 admin.site.register(Mechanical)
 admin.site.register(Electrical)
+admin.site.register(weeklyExpenses)
+admin.site.register(tyres)
+admin.site.register(dieselFilling)
